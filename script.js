8216c4050d20c77decf84a161d56d2850910ccfe
vadrif-draco <55457021+vadrif-draco@users.noreply.github.com>
2019-09-21 20:49:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var sound_notice = '<p id="sound-notice" onanimationend="game_screen.innerHTML = start_prompt">Please turn sound on for best experience.</p>';
+var sound_notice = '<p id="sound-notice" onanimationend="game_screen.innerHTML = start_prompt">Please use headphones for best experience.</p>';
+
+var sound_notice_element = document.getElementById("sound-notice");
+var game_screen = document.getElementById("game-screen");
+const sound = new Audio('');
 
 var start_prompt = '\
 <button class = "retry" onclick = "start_game()">Begin</p>\
 '
     // To be fair, the "retry" class that I used for the try again button was very fitting so I just reused it here lol.
+function start_game() {
+    game_screen.innerHTML = welcome;
+    sound.src = 'intro.mp3';
+    sound.play();
+}
+
 var welcome = '\
 <p class="welcome w1">Hi,</p>\
 <p class="welcome w2">Welcome to Shroofland.</p>\
@@ -19,32 +29,57 @@ var stage1 = '\
 placeholder="Think fast, Shroof!" />\
 '
 
-var failure = '\
-<p class = "failure" onanimationend="game_screen.innerHTML = retry;"><b>YOU<br/>FAILED!</b></p>\
-'
-
-var retry = '\
-<button class = "retry" onclick = "restart_game()">Try again</p>\
-'
-
 var stage1_passed = false;
-const sound = new Audio('');
-var game_screen = document.getElementById("game-screen");
-var sound_notice_element = document.getElementById("sound-notice");
-var input1 = document.getElementById("input1");
-
-function start_game() {
-    game_screen.innerHTML = welcome;
-    sound.src = 'intro.mp3';
-    sound.play();
-}
 
 function stage1_check() {
     if (document.getElementById("input1").value == "12") {
         stage1_passed = true;
-        game_screen.innerHTML = welcome;
+        game_screen.innerHTML = passage1;
     }
 }
+
+var passage1 = '\
+<p class = "text1_pass" style="font-size: 1.5em;">What is <b style="color:white;">7 </b>+<b style="color:white;"> 5</b> ?</p>\
+<br/>\
+<input class = "input1_pass" type="number" value="12" disabled onanimationend="stage2_start()"/> \
+'
+
+function stage2_start() {
+    game_screen.innerHTML = stage2
+    sound.src = 'stage2.mp3';
+    sound.play();
+}
+var stage2 = '\
+<p style="font-size: 1.5em;">What is your favorite <br/><b style="color:white;">whale family</b>?</p>\
+<br/>\
+<div class = "stage2_choices_container">\
+<button class = "stage2_btn c1" onclick = "failure_check(stage2_passed)">Balaenopteridae</p>\
+<button class = "stage2_btn c2" onclick = "failure_check(stage2_passed)">Balaenidae</p>\
+<button class = "stage2_btn c3" onclick = "failure_check(stage2_passed)">Cetotheriidae</p>\
+<button class = "stage2_btn c4" onclick = "failure_check(stage2_passed)">Eschrichtiidae</p>\
+<button class = "stage2_btn c5" onclick = "stage2_pass()">Monodontidae.. dee?</p>\
+<button class = "stage2_btn c6" onclick = "failure_check(stage2_passed)">Physeteridae</p>\
+<button class = "stage2_btn c7" onclick = "failure_check(stage2_passed)">Kogiidae</p>\
+<button class = "stage2_btn c8" onclick = "failure_check(stage2_passed)">Ziphiidae</p>\
+</div>\
+'
+
+var stage2_passed = false;
+
+function stage2_pass() {
+    stage2_passed = true;
+    game_screen.innerHTML = passage2;
+}
+
+var passage2 = '<video autoplay width="360" preload="auto"\
+<source src="Monodontidae.mp4" type="video/mp4">\
+</video>'
+
+
+
+var failure = '\
+<p class = "failure" onanimationend="game_screen.innerHTML = retry;"><b>YOU<br/>FAILED!</b></p>\
+'
 
 function failure_check(stage_passed) {
     if (stage_passed == false) {
@@ -54,10 +89,20 @@ function failure_check(stage_passed) {
     }
 }
 
+var retry = '\
+<button class = "retry" onclick = "restart_game()">Try again</p>\
+'
+
 function restart_game() {
     stage1_passed = false;
     // same for all stages please
     game_screen.innerHTML = stage1;
 }
 
-window.onload = () => game_screen.innerHTML = sound_notice;
+// window.onload = stage2_start(); // Development bypass
+window.onload = load_game(); // Actual start
+
+function load_game() {
+
+    game_screen.innerHTML = sound_notice;
+}
